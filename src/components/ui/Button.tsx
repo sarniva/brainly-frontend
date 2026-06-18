26132e1d06549d03588e715ebc13c69313f47da0
Sarnvia Das <sarnivadas@gmail.com>
2026-06-18 10:47:27 +0530

    Add buttons

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,6 @@
+type Variants = "primary" | "secondary";
 export interface ButtonProps {
-  variant: "primary" | "secondary";
+  variant: Variants;
   size: "sm" | "md" | "lg";
   text: string;
   startIcon?: any;
@@ -7,8 +8,27 @@ export interface ButtonProps {
   onClick: () => void;
 }
 
-export const Button = (props:ButtonProps) => {
-  return <button></button>;
+const variantStyles = {
+  primary: "bg-purple-600 text-black",
+  secondary: "bg-purple-300 text-purple-600",
+};
+const sizeStyles = {
+  sm: "py-1 px-2",
+  md: "py-2 px-4",
+  lg: "py-4 px-6",
+};
+
+const defaultStyles = "rounded-md flex";
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <button
+      className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}
+    >
+      {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null}{" "}
+      {props.text} {props.endIcon}
+    </button>
+  );
 };
 
 //<Button variant="primary" size="md" onClick={} text={} startIcon={}/>
